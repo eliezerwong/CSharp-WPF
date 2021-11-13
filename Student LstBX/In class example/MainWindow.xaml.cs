dd04b783809b47a615ef50b68a8d188da26b0b88
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 22:08:44 -0600

    Add files via upload

---
+++ Student LstBX/In class example/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace In_class_example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbxid_Copy6_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string id, fName, lName, probation, gpa, balance;
+            
+            id = txtid.Text;
+            fName = txtFN.Text;
+            lName = txtLN.Text;
+            //probation = txtProbation.Text;
+            gpa = txtGPA.Text;
+            balance = txtBB.Text;
+
+            Student stud = new Student(Convert.ToInt32(id), fName, lName, Convert.ToDouble(balance));
+            stud.GPA = Convert.ToDouble(gpa);
+            stud.IsOnProbation = chkProbation.IsChecked.Value;
+            //if (probation.ToLower() == "yes")
+            //{
+            //    stud.IsOnProbation = true;
+            //}
+            //else
+            //{
+            //    stud.IsOnProbation = false;
+            //}
+
+            Student.Items.Add(stud);
+
+
+        }
+
+        private void Student_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student selectedStudent = (Student)Student.SelectedItem;
+
+            MessageBox.Show($"{selectedStudent.FirstName} has a {selectedStudent.GPA.ToString("N0")} GPA");
+        }
+    }
+}
---
+++ Student LstBX/In class example/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace In_class_example
+{
+    public class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        
+        private double BursarBalance;
+
+        //public List<string> Majors { get; set; }
+        //public UniversityCourse course { get; set; }
+
+        /// <summary>
+        /// Default/Empty Constructor (setting above values to a default)
+        /// </summary>
+        //ctor + tab + tab (shortcut)
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+
+            //Majors = new List<string>();
+            //course = new UniversityCourse();
+        }
+
+        /// <summary>
+        /// Overload Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fName"></param>
+        /// <param name="lName"></param>
+        /// <param name="BursarBalance"></param>
+        public Student(int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+
+        }
+
+        public void MakePayment(double amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("invalid");
+            }
+
+            BursarBalance -= amount;
+            //BursarBalance = BursarBalance - amount
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} ({SoonerID.ToString("N0")})";
+        }
+
+    }
+
+    //public class UniversityCourse
+    //{
+
+    //}
+}
