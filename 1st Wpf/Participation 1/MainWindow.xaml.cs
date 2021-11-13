c9a73c174cbf15daac959e8c9c9c75db0ded2093
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 22:10:32 -0600

    Add files via upload

---
+++ 1st Wpf/Participation 1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Participation_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lblOutput.Visibility = Visibility.Hidden;
+
+
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string dob = txtDOB.Text;
+            DateTime birthDate = Convert.ToDateTime(dob);
+            TimeSpan age = DateTime.Now - birthDate;
+
+            string name = txtName.Text;
+
+
+
+            //MessageBox.Show((age.TotalDays / 365).ToString());
+
+            double old = age.TotalDays / 365;
+            lblOutput.Content = $"Hello {name}, you are {old.ToString("N0")}";
+            lblOutput.Visibility = Visibility.Visible;
+        }
+
+        private void btnSubmit_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Console.BackgroundColor = ConsoleColor.DarkYellow;
+            //Window.Background = Brushes.Yellow;
+        }
+    }
+}
---
+++ JsonChuckNorris/JsonChuckNorris/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace JsonChuckNorris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboJokes.Items.Add("All");
+            cboJokes.SelectedIndex = 0;
+
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://api.chucknorris.io/jokes/categories").Result;
+
+                List<string> api = JsonConvert.DeserializeObject<List<string>>(jsonData);
+
+                foreach (var item in api)
+                {
+                    if (string.IsNullOrWhiteSpace(item) == true)
+                    {
+                        return;
+                    }
+
+                    if (!cboJokes.Items.Contains(item))
+                    {
+                        cboJokes.Items.Add(item);
+                        Categories cat = new Categories();
+                        cat.Category.Add(item);
+                    }
+                }
+            }
+
+
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+            string category = cboJokes.SelectedValue.ToString();
+
+            string url = $"https://api.chucknorris.io/jokes/random?category={category}";
+
+            using (var client = new HttpClient())
+            {
+                string jsonJokes = client.GetStringAsync("https://api.chucknorris.io/jokes/random?category={Category}").Result;
+
+                var apiJokes = JsonConvert.DeserializeObject<List<string>>(jsonJokes);
+
+                if (cboJokes.SelectedValue.ToString().ToLower() == "all")
+                {
+                    string jsonAll = client.GetStringAsync("https://api.chucknorris.io/jokes/random").Result;
+                    var apiAll = JsonConvert.DeserializeObject<List<string>>(jsonAll);
+                    txbJoke.Text = apiAll.ToString();
+                }
+                else if (apiJokes.Contains())
+                {
+
+                }
+
+            }
+        }
+    }
+}
