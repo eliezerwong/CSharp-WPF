c153ffd6c935f83683b1ebd6f70ffdc42506b7b9
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 22:13:00 -0600

    Add files via upload

---
+++ WPFContactList/WPFContactList/Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFContactList
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Image { get; set; }
+
+
+        public Contact()
+        {
+            ID = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Image = string.Empty;
+        }
+
+        public Contact(int id, string first, string last, string email, string image)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            Email = email;
+            Image = image;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName},{FirstName}";
+        }
+    }
+
+
+}
---
+++ WPFContactList/WPFContactList/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WPFContactList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //string path = Environment.GetEnvironmentVariable("USERPROFILE") + @"\Downloads";
+
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.InitialDirectory = path;
+            //ofd.ShowDialog();
+
+            string[] lines = File.ReadAllLines("contacts.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                //0      1          2           3       4
+                //Id | FirstName | LastName | Email | Photo
+                
+                string[] pieces = lines[i].Split('|');
+                Contact c = new Contact(Convert.ToInt32(pieces[0]), pieces[1], pieces[2], pieces[3], pieces[4]);
+                //c.FirstName = pieces[1];
+                //c.LastName = pieces[2];
+                //c.Email = pieces[3];
+                //c.Image = pieces[4];
+
+                lstContacts.Items.Add(c);
+
+            }
+        }
+
+        
+        private void btnFilePath_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+          
+        }
+    }
+}
