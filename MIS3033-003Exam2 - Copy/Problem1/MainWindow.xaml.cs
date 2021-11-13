1dea1b7df22bcb0b24918971c5b0e0db614a5b70
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 22:15:33 -0600

    Add files via upload

---
+++ MIS3033-003Exam2 - Copy/Problem1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace Problem1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// MIS 3033-003 Exam 2
+    /// Eliezer Emmanuel Wong (113452682)
+    public partial class MainWindow : Window
+    {
+        //List<Restaurants> allRes = new List<Restaurants>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //get restaurant names
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("http://pcbstuou.w27.wh-2.com/webservices/3033/api/restaurants/names").Result;
+                Restaurants api = JsonConvert.DeserializeObject<Restaurants>(jsonData);
+
+                //add restaurants to list
+                foreach (var item in api.restaurants)
+                {
+                    lstRestaurants.Items.Add(item);
+                }
+
+            }
+        }
+
+        private void lstRestaurants_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //when restaurant selected
+            RestaurantInfo selectedRestaurant = (RestaurantInfo)lstRestaurants.SelectedItem;
+            //concactanating restaurant id in url
+            string url = $"http://pcbstuou.w27.wh-2.com/webservices/3033/api/restaurants?id={selectedRestaurant.id}";
+
+            //get the 2nd api using the concactanated id
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync(url).Result;
+                RestaurantInformation infoApi = JsonConvert.DeserializeObject<RestaurantInformation>(jsonData);
+                
+                //show the new window
+                WndInfo wnd = new WndInfo();
+                wnd.ShowInfo(infoApi);
+                wnd.ShowDialog();
+            }
+
+
+        }
+    }
+}
